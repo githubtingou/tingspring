d5619f2382b708918f8c94b95430f6f57998790f
lishuang <ShuangLiSH@wistronits.com>
2019-10-24 15:12:18 +0800

    spring boot @ConfigurationProperties 初步体验

--- src/main/java/com/ting/tingspring/controller/UserController.java
+++ src/main/java/com/ting/tingspring/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.ting.tingspring.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ting.tingspring.dto.UserDto;
+import com.ting.tingspring.spring.PropertisCustom;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+
+    @Value("${ting.age}")
+    private int age;
+
+    @Value("${ting.name}")
+    private String name;
+
+    @Autowired
+    private PropertisCustom propertisCustom;
+
     /**
      * 获取用户信息
      *
@@ -24,6 +38,9 @@ public class UserController {
      */
     @RequestMapping(value = "getUser")
     public UserDto getUser(@RequestBody UserDto userDto) {
+        log.info("hashMap--->{}", JSON.toJSONString(propertisCustom.getHashMap()));
+        log.info("list--->{}", JSON.toJSONString(propertisCustom.getList()));
+        log.info("email-->{}", JSON.toJSONString(propertisCustom.getEmail()));
         return userDto;
     }
 
---
+++ src/main/java/com/ting/tingspring/spring/PropertisCustom.java
@@ -0,0 +1,28 @@
+package com.ting.tingspring.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 自定义配置信息
+ *
+ * @author LISHUANG
+ * @date 2019/10/24
+ */
+@Component
+@ConfigurationProperties(prefix = "ting")
+@Getter
+@Setter
+public class PropertisCustom {
+    private String name;
+    private Integer age;
+    private HashMap<String, Object> hashMap;
+    private List<String> list;
+    private String email;
+}
